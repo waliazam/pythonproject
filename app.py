e0fdaa761b4af032bc090956968c200def7804a6
Wali Azam <wali.azam13@gmail.com>
2023-08-17 13:03:04 +0500

    Code day 2 basics of python

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 print("Welcome to 2nd class of python")
-
+#welcome message
 name = input("Enter Your name: ")
 print("Hello " + name)
 
 birth_year = input("Enter your birth Year: ")
+# culculate age 
 age = 2023 - int(birth_year)
+
+# display message 
 print("You are " + str(age) + " Years old.")
